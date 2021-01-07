ebe04ce84538964c4b07852682e243f0155a988f
ConArrr <conarrr7714@gmail.com>
2021-01-08 00:47:10 +0900

    Reset.

--- functions/src/services/mangarel/constants.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const collectionName = {
-  authors: 'authors',
-  books: 'books',
-  users: 'users',
-  publishers: 'publishers',
-  docCounters: 'docCounters',
-  feedMemos: 'feedMemos',
-} as const
